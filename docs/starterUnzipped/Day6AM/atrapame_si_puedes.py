31802a889c4ced813e0bffcd434bdc9dc8208d66
Ali Malik <malikali@cs.stanford.edu>
2022-07-05 08:58:49 -0500

    day6

---
+++ docs/starterUnzipped/Day6AM/atrapame_si_puedes.py
@@ -0,0 +1,29 @@
+"""
+File: atrapame_si_puedes.py
+-------------------
+"""
+
+from graphics import Canvas
+import random
+
+# Tamano de los cuadrados negro
+TAMANO_CUAD_NEGRO = 50
+
+# Cuantas cuadrados negros debemos poner a la pantalla 
+N_CUADRADOS = 20
+
+# Tamano del cuadrado azul que el usuario esta tratando de tocar 
+TAMANDO_CUAD_AZUL = 60
+
+
+def main():
+    canvas = Canvas()
+    canvas.set_canvas_title("Atrápame si puedes")
+
+    # TODO: su codigo aca
+
+    canvas.mainloop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ docs/starterUnzipped/Day6AM/ubicacion_mouse.py
@@ -0,0 +1,19 @@
+"""
+File: ubicacion_mouse.py
+-------------------
+"""
+
+from graphics import Canvas
+
+
+def main():
+    canvas = Canvas()
+    canvas.set_canvas_title("Ubicacion de Mouse")
+
+    # TODO: su codigo aca
+
+    canvas.mainloop()
+
+
+if __name__ == "__main__":
+    main()
